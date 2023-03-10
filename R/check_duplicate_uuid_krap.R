a98621aeff5cce3e2b4626cb0e2b331977b390a4
Ahmed-REACH <ahmed.slama@reach-initiative.org>
2023-03-10 16:47:14 +0700

    Initial Commit
    This is the initial commit. contains 1 function for data quality

---
+++ R/check_duplicate_uuid_krap.R
@@ -0,0 +1,66 @@
+check_duplicate_uuid_krap <- function(data = NULL,
+                       uuid_column=NULL,
+                       survey_consent=NULL,
+                       details=c(enumerator_id, uuid_column, survey_date),
+                       deleteIsuniquerespondantIDDuplicated=FALSE){
+  if(is.null(data) | nrow(data)==0 | !is.data.frame(data)){
+    stop("Please provide the dataset")
+  }
+  if(is.null(survey_consent) | !is.character(survey_consent)){
+    stop("Please provide the column name of ´survey consent´")
+  }
+  if(is.null(uuid_column) | !is.character(uuid_column) | is.na(match(uuid_column,names(data)))){
+    stop("Please provide the column name of ´unique survey ID´")
+  }
+  if(is.null(details) | !is.character(details)){
+    stop("Please provide the columns you want in the result (include the enumerator id column if you want to check by enumerator)")
+  }
+  if(is.null(deleteIsuniquerespondantIDDuplicated) | !is.logical(deleteIsuniquerespondantIDDuplicated)){
+    stop("Please provide the delete action to be done (TRUE/FALSE)")
+  }
+
+  if(deleteIsuniquerespondantIDDuplicated){
+    data[,survey_consent][duplicated(data[,uuid_column])] <- "deletedIsuniquerespondantIDDuplicated"
+  }
+
+  # TO BE BE CHANGED WITH DYNAMIC COLUMS
+  ret_log <- subset(data,duplicated(data[,uuid_column])) %>%
+    dplyr::select(all_of(details), survey_consent=survey_consent)
+
+  check <- data.frame(categories=c("OK", "NOK"),
+                      Nb = c(  length(data[,1])-length(ret_log[,1]),
+                               length(ret_log[,1])))
+
+
+  t1 <- check
+  t1$fraction <- t1$Nb / sum(t1$Nb)
+  t1 <- t1[order(t1$fraction), ]
+  t1$ymax <- cumsum(t1$fraction)
+  t1$ymin <- c(0, utils::head(t1$ymax, n=-1))
+
+  graph  <- ggplot2::ggplot(t1, ggplot2::aes(fill=categories,
+                                             ymax=ymax,
+                                             ymin=ymin,
+                                             xmax=4,
+                                             xmin=3)) +
+    ggplot2::geom_rect(colour="grey30") +
+    ggplot2::coord_polar(theta="y") +
+    ggplot2::xlim(c(1, 4)) +
+    ggplot2::theme_void() +
+    ggplot2::theme(panel.grid=ggplot2::element_blank()) +
+    ggplot2::theme(axis.text=ggplot2::element_blank()) +
+    ggplot2::theme(axis.ticks=ggplot2::element_blank()) +
+    ggplot2::theme(legend.position='none') +
+    ggplot2::labs(title= "Are Unique ID Duplicated?",
+                  subtitle = paste0("Errors: ", round(t1$fraction[t1$categories == 'NOK']*100,2), "%"))
+
+
+
+  result <- list( data = data, #  same dataset as the inputed one but with survey marked for deletion if errors are found and delete=TRUE (or NULL)
+                  ret_log  = ret_log , # list of the errors found otherwise NULL
+                  var = NULL, #  a list of value (or NULL)
+                  graph = graph) # graphical representation of the results (or NULL)
+
+
+  return(result)
+}
---
+++ R/find_different_columns.R
@@ -0,0 +1,65 @@
+#' Find difference between two submitted surveys
+#'
+#' This function returns a string containing all column names where values are different between surveys.
+#' The function takes as arguments a dataframe, name of the unique identifier column, and 2 survey ID (1 mandatory, 1 optional).
+#'
+#'
+#'
+#' Note: if you are comparing two surveys with the same identifier, only one identifier is needed.
+#'
+#'
+#' @param data dataframe containing survey data and unique identifier column for the surveys.
+#' @param identifier column name of the survey ID. Default: ´identifier´.
+#' @param value the survey ID in question. Used as a key for filtering out surveys for comparison.
+#' @param value2 optional parameter defining a different survey ID than the first. Useful for comparing surveys with different identifiers.
+#'
+#'
+#' @return A character string containing the names of the columns that are different. column names are separated with a comma ´,´ within the string.
+#' @export
+#'
+#' @examples
+#' # Get the names of questions that have different responses on surveys. Fetching by 1 identifier i.e. surveys with duplicate identifier.
+#' \code{n} names <- find_different_columns(data, "identifier", "3681f44c-8f1f-49b0-1dzc-6f741d087b24")
+#'
+#' # Get the names of questions that have different responses on surveys. Fetching by 2 different identifiers
+#' \code{n} names <- find_different_columns(data, "identifier", "3681f44c-8f1f-49b0-1dzc-6f741d087b24", "a211f44a-9d1f-94b0-10dc-6f741d811b29")
+
+
+
+find_different_columns <- function(data,
+                                        identifier="identifier",
+                                        value,
+                                        value2=NULL) {
+
+  if(is.null(data) | nrow(data)<=1 | !is.data.frame(data)){
+    stop("Please provide the dataset. Dataset should contain at least 2 surveys/rows")
+  }
+  if( !is.character(identifier) | (is.null(identifier) & is.na(match("identifier",names(data)))) | (!is.null(identifier) & is.na(match(identifier,names(data)))) ){
+    stop("Please provide the column name of ´survey ID´")
+  }
+  if( !is.character(value) | is.null(value) | is.na(match(value,data[[identifier]])) ){
+    stop("Please provide the ID of the survey in question. E.g. ´2554f55-88988ff-qan22rr27-p66abhfb1´")
+  }
+  if( (is.null(value2) & length(which(data[[identifier]]==value))<2) | (!is.null(value2) & !is.character(value2)) | (!is.null(value2) & length(which(data[[identifier]]==value2))<1) ){
+    stop("Please provide the ID of the second survey in question. It could be that the first provided ID is not duplicated or the second provided ID does not exist in the column")
+  }
+
+  # Select the two rows and transpose the resulting dataframe
+  working_data <- data %>%
+    subset(identifier %in% c(value, value2)) %>%
+    t()
+
+
+  # Fix failure in transposing the colnames (question names)
+  rownames(working_data)<- colnames(data)
+  #NB: Did not change the column name to identifier because R would add ..1 ..2 to the column name (because identifier values can be duplicated)
+
+
+  # Get the names of the questions that have different values from the first survey
+  col_diff <- rownames(working_data)[apply(working_data, 1, function(x){any(x != x[1])})]
+
+  # Return the column names as a string (can use punctuation to find out how many different columns)
+  return(paste0(paste(col_diff, collapse = ", "),"."))
+}
+
+# devtools::document()
